c3f0349d1ed47311ba265a1b58b1372c8ac6a2de
NathanBurrows8 <58235914+NathanBurrows8@users.noreply.github.com>
2021-12-21 18:54:48 +0000

    do love python changing completely and Pycharm saying it wone let me use Python 2

---
+++ KnowledgeEngine.py
@@ -0,0 +1,5 @@
+#the original plan was to use PyKE but this only works on Python 2.x and is deprecated - as this had the most documentation/was most popular
+#now the plan is to use Experta which is a fork for PyKnow (also deprecated). Can be pip installed
+#Experta is a Python library based on the CLIPS programming language.
+
+
