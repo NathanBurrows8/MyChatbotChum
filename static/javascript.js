809f208683acb9298d96eb18b4ad4a520ef06cf2
Melodic22 <58524577+Melodic22@users.noreply.github.com>
2022-01-22 04:08:58 +0000

    added test harness to javascript.js called myTester(). calls function sendToPythonAndWait(text) with different inputs and then compares to the outputs to find if the test was successful or not. Lists of inputs and expected outputs are included in the myTester() function as lists of lists.

--- static/javascript.js
+++ static/javascript.js
@@ -60,6 +60,7 @@ function onSendPressed() {
         textBoxInput.value = "";
     }
 }
+
     //This is the speech recognition function. Some extra CSS and UI tweaks are changed while/after recognition.
     //The resulting text is injected into the message box.
 function micAnalysis() {
@@ -74,6 +75,7 @@ function micAnalysis() {
         micRunning = true;
         removeTextGlow();
     }
+
     recognition.onend = () => {
         document.getElementById('micIcon').src = "/static/mic.png";
         document.getElementById('textBoxInput').style.width = '602px';
@@ -98,6 +100,8 @@ function micAnalysis() {
             recognition.stop();
         }
     }
+
+    myTester()
 }
     //Removing the CSS from the above function when the user sends the message or clicks in a certain area
 function removeTextGlow() {
@@ -139,5 +143,104 @@ function volumeButtonClicked() {
     }
 
 }
+
+function sendToPythonAndWait(text)
+{
+    return new Promise((resolve, reject) =>
+    {
+        sendToPython(text);
+        socket.on('message_from_python', function(msg) {
+            resolve(msg);
+        })
+    });
+}
+
+async function myTester() {
+    console.log("myTester called")
+
+    {
+        const response = await sendToPythonAndWait("book a single ticket friday 10pm from northampton to london");
+        if (response === "------------------------FOR 28/01/22-----------------------<br> The cheapest journey departs from Northampton</mark> at 22:07, and arrives at London Euston at 23:30.<br>The journey will take 1 hour  and 23 minutes, and has 0 changes.<br>The ticket will cost £9.50.<br> To view your booking, <a href=\"https://ojp.nationalrail.co.uk/service/timesandfares/northampton/london/280122/2200/dep\" target=\"_blank\"> click here.</a> <br> (Journey provided by London Northwestern Railway)<br>") {
+            console.log("Test 1 Passed")
+        } else {
+            console.log("Error: Unexpected Response:\n" + msg + "\n")
+        }
+    }
+
+    {
+        const response = await sendToPythonAndWait("book a single ticket friday 10pm from london to northampton");
+        if (response === "------------------------FOR 28/01/22-----------------------<br> The cheapest journey departs from London Euston</mark> at 22:18, and arrives at Northampton at 23:48.<br>The journey will take 1 hour  and 30 minutes, and has 0 changes.<br>The ticket will cost £9.50.<br> To view your booking, <a href=\"https://ojp.nationalrail.co.uk/service/timesandfares/london/northampton/280122/2200/dep\" target=\"_blank\"> click here.</a> <br> (Journey provided by London Northwestern Railway)<br>") {
+            console.log("Test 2 Passed");
+        } else {
+            console.log("Error: Unexpected Response:\n" + msg + "\n")
+        }
+    }
+
+    {
+        // TESTING VALID GREETING INPUTS, CREATE SEPARATE TESTS FOR INVALID (unable_to_parse) INPUTS
+        /*      helloRegex = [
+        {"LOWER": {"REGEX": "hi(?![\S])|hey(?![\S])|yo(?![\S])|hello(?![\S])"}}*/
+
+        const listsOfResponses = [
+            ["Hello there! What assistance do you need?", "Hi there! How can I help?", "Nice to see you! What can I do for you?", "Hello, can i help?", "Hi! Is there anything you would like my help with?"],
+            ["Will this be a single, or a return ticket?"]
+            //add more response lists here
+        ]
+
+        const listsofInputs = [
+            ["hi", "hey", "yo", "hello", "hello my name is Bob", "Hi I need some help", "this should fail"],
+            ["booking", "book", "ticket", "buy", "i'd like to book a ticket please"]
+            //add more input lists for the corresponding response list in listsOfResponses
+        ]
+
+
+        for (let list = 0; list < listsofInputs.length; list++) {
+            for (let input = 0; input < listsofInputs[list].length; input++) {
+                let success = false
+                const actualResponse = await sendToPythonAndWait(listsofInputs[list][input]);
+                //console.log("list=" + list + " input=" +input)
+                //console.log("current input: " +listsofInputs[list][input])
+                for (let response = 0; response < listsOfResponses[list].length; response++ ) {
+                    //console.log("listsofResponses[" + list + "][" + response + "]")
+                    //console.log("comparison response: " + listsofResponses[list][response])
+                    //console.log("actual response: "+actualResponse)
+                    if (actualResponse === listsOfResponses[list][response]) {
+                        //console.log("Test " + list + "." + input + " Passed with User Input: \"" + listsofInputs[list][input] + "\" and Bot Response: \"" + actualResponse + "\".")
+                        success = true
+                    }
+                }
+
+                if (success === true) {
+                    console.log("Test " + list + "." + input + " Passed with User Input: \"" + listsofInputs[list][input] + "\" and Bot Response: \"" + actualResponse + "\".")
+                } else {
+                    console.log("ERROR: Test " + list + "." + input + " Failed with User Input:  \"" + listsofInputs[list][input] + "\" and Bot Response: \"" + actualResponse + "\".")
+                }
+            }
+        }
+
+    }
+
+    //{
+/*        const helloGreeting = ["hi", "hey", "yo", "hello", "hello my name is Bob", "Hi I need some help"]
+
+        for (let i in helloGreeting) {
+            const response = await sendToPythonAndWait(helloGreeting[i]);
+
+            //console.log(response)
+
+            if (response === "Hello there! What assistance do you need?" || response === "Hi there! How can I help?" || response === "Nice to see you! What can I do for you?" || response === "Hello, can i help?" || response === "Hi! Is there anything you would like my help with?") {
+                console.log("Test 3."+ i + " Passed with User Input: \"" + helloGreeting[i] + "\" and Bot Response: \"" + response + "\".")
+            }
+            else {
+                console.log("ERROR: Test 3." + i + " Failed with User Input:  \"" + helloGreeting[i] + "\" and Bot Response: \"" + response + "\".")
+                // console.log("Error: Unexpected Response:\n" + response + "\n")
+            }
+            i++
+        }*/
+    //}
+}
+
+
+
 
 
